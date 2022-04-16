e7c7fddd7d52fd7f343c2da1362c5da8a9dd38dd
Christian Girling <girling.christian@gmail.com>
2022-04-16 12:07:05 -0700

    Create server

---
+++ node-backend/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mydatabase")
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err.reason);
+  });
+
+const bookRoute = require("./routes/book.routes");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(cors());
+
+// Static directory path
+app.use(express.static(path.join(__dirname, "dist/angular-mean-app")));
+
+// API Root
+app.use("/api", bookRoute);
+
+// PORT
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Base route
+app.get("/", (req, res) => {
+  res.send("invalid endpoint");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/angular-mean-app/index.html"));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
